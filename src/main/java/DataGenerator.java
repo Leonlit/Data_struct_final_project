82cb5ef118427a5234f8d6d09ba4ec65f43b552a
Leon <leonlit123@gmail.com>
2021-06-15 18:17:50 +0800

    WIP

--- src/main/java/DataGenerator.java
+++ src/main/java/DataGenerator.java
@@ -1,3 +1,8 @@
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +16,66 @@
 
 //generate data from the json data (random everytime)
 public class DataGenerator {
+    
+    final static public Book[] generateBooks () {
+        Book[] books = new Book[20]; 
+        try {
+            JSONObject data = new JSONObject(DataReader.getRecords());
+            int bookID[] = getJsonArrayIntValue(data.getJSONArray("writers"));
+            String bookName[] = getJsonArrayStringValue(data.getJSONArray("bookName"));
+            String writers[] = getJsonArrayStringValue(data.getJSONArray("writers"));
+            String dates[] = getJsonArrayStringValue(data.getJSONArray("dates"));
+            String categories[] = getJsonArrayStringValue(data.getJSONArray("categories"));
+            String bookCategories[] = getJsonArrayStringValue(data.getJSONArray("bookCategories"));
+            String students[] = getJsonArrayStringValue(data.getJSONArray("students"));
+            String waitingList[][] = getBookWaitingList(data.getJSONArray("waitingList"));
+            for (int idx=0;idx < bookID.length;) {
+                String currBookCategories = generateCategories(categories, bookCategories[idx]);
+                
+            }
+        }catch (JSONException ex) {
+            System.out.println("JSON file is not valid!!!");
+            ex.printStackTrace();
+        }
+        
+        
+        return books;
+    }
+    
+    final static private String[][] getBookWaitingList (JSONArray arr) {
+        String results[][] = new String[20][7];
+        try {
+            for (int idx = 0;idx < arr.length();idx++) {
+                results[idx] = getJsonArrayStringValue(arr.getJSONArray(idx));
+            }
+        }catch (JSONException ex) {
+            System.out.println("JSON file is not valid!!!");
+        }
+        return results;
+    }
+    
+    final static private String[] getJsonArrayStringValue (JSONArray arr) {
+        String results[] = new String[arr.length()];
+        try {
+            for (int idx = 0;idx < arr.length();idx++) {
+                results[idx] = arr.getString(idx);
+            }
+        }catch (JSONException ex) {
+            System.out.println("JSON file is not valid!!!");
+        }
+        return results;
+    }
+    
+    final static private int[] getJsonArrayIntValue (JSONArray arr) {
+        int results[] = new int[arr.length()];
+        try {
+            for (int idx = 0;idx < arr.length();idx++) {
+                results[idx] = arr.getInt(idx);
+            }
+        }catch (JSONException ex) {
+            System.out.println("JSON file is not valid!!!");
+        }
+        return results;
+    }
     
 }
--- src/main/java/DataReader.java
+++ src/main/java/DataReader.java
@@ -3,45 +3,13 @@ import java.io.IOException;
 import java.util.Scanner;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DataReader {
     
-    final static public Book[] getRecords () {
+    final static public String getRecords () {
         String result = readFileContent();
-        System.out.println(result);
-        Book books[] = new Book[1];
-        try {
-            
-            JSONArray data = new JSONArray(result);
-            books = new Book[data.length()];
-            for (int x = 0; x< data.length();x++) {
-                    int id = data.getJSONObject(x).getInt("id");
-                    String name = data.getJSONObject(x).getString("name");
-                    String writer = data.getJSONObject(x).getString("writer");
-                    String date = data.getJSONObject(x).getString("date");
-                    String categories[] = getJsonArrayValue(data.getJSONObject(x).getJSONArray("categories"));
-                    String waitingList[] = getJsonArrayValue(data.getJSONObject(x).getJSONArray("waiting-list"));
-                    String history[] = getJsonArrayValue(data.getJSONObject(x).getJSONArray("previously-borrowed"));
-                    Book temp = new Book(id, name, writer, date, categories, waitingList, history);
-                    books[x] = temp;
-            }
-        }catch (JSONException ex) {
-            System.out.println("JSON file is not valid!!!");
-            ex.printStackTrace();
-        }
-        return books;
-    }
-    
-    final static private String[] getJsonArrayValue (JSONArray arr) {
-        String results[] = new String[arr.length()];
-        try {
-            for (int idx = 0;idx < arr.length();idx++) {
-                results[idx] = arr.getString(idx);
-            }
-        }catch (JSONException ex) {
-            System.out.println("JSON file is not valid!!!");
-        }
-        return results;
+        return result;
     }
     
     final static private String readFileContent () {
